e6dcecef523247a718a42ef8cb62ec8be4ebfd05
Bruno Foggia <brunnofoggia@gmail.com>
2025-09-23 15:14:45 -0300

    build(npm): dep up

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 0,
         // '@typescript-eslint/explicit-function-return-type': 0,
         // '@typescript-eslint/explicit-module-boundary-types': 0,
-        // '@typescript-eslint/no-floating-promises': 0,
+        '@typescript-eslint/no-floating-promises': 2,
         // temporary
         // '@typescript-eslint/no-unsafe-assignment': 0,
         // '@typescript-eslint/no-unsafe-member-access': 0
